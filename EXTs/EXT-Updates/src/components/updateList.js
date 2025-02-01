7df3d25859ec14ea94d798df7fa11c17edf18a9c
bugsounet <github@bugsounet.fr>
2025-02-01 11:54:39 +0100

    fix: updateList

--- EXTs/EXT-Updates/src/components/updateList.js
+++ EXTs/EXT-Updates/src/components/updateList.js
@@ -1,13 +1,5 @@
 const updateList = [
-  "MMM-FranceInfo",
-  "MMM-Freebox",
-  "MMM-Glassy",
-  "MMM-GoogleAssistant",
-  "MMM-MeteoFrance",
-  "MMM-NetatmoThermostat",
-  "MMM-Pir",
-  "MMM-PrixCarburants",
-  "MMM-Saint"
+  "MMM-Bugsounet"
 ];
 
 module.exports = updateList;
