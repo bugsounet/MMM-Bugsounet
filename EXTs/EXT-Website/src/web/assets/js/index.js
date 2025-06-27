e8101c4afa77fa14d39d392cbb8002437a785722
bugsounet <github@bugsounet.fr>
2025-06-27 09:18:43 +0200

    EXT-Website: delete Swal on success full login

--- EXTs/EXT-Website/src/web/assets/js/index.js
+++ EXTs/EXT-Website/src/web/assets/js/index.js
@@ -72,23 +72,8 @@ document.addEventListener("Includes_Complete", async () => {
       let credentials = `${document.getElementById("username").value}:${document.getElementById("password").value}`;
       let encodedCredentials = btoa(credentials);
       doLogin(encodedCredentials, (response) => {
-        Swal.fire({
-          position: "center",
-          icon: "success",
-          title: `${loginTranslations["Welcome"]} ${document.getElementById("username").value}`,
-          showConfirmButton: false,
-          timer: 1200,
-          didOpen: () => {
-            document.getElementById("wrapper").classList.add("blur");
-          },
-          allowOutsideClick: false,
-          allowEscapeKey: false
-        }).then((result) => {
-          if (result.isDismissed) {
-            localStorage.setItem("MMM-Bugsounet", JSON.stringify(response.session));
-            location.href = "/";
-          }
-        });
+        localStorage.setItem("MMM-Bugsounet", JSON.stringify(response.session));
+        location.href = "/";
       }, (err) => {
         document.getElementById("username").value = "";
         document.getElementById("password").value = "";
