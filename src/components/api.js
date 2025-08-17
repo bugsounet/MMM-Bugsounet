601cbdcb65ebf6f5b8c357c45b419e1edf89c575
bugsounet <github@bugsounet.fr>
2025-08-17 13:01:46 +0200

    Temp display link for direct test

--- src/components/api.js
+++ src/components/api.js
@@ -198,6 +198,7 @@ class api {
     if (AdminActivate && AdminActivate.level === 10 && AdminActivate.disabled) {
       console.warn(`[Bugsounet] [API] For activate your ${AdminActivate.username} account`);
       console.warn("[Bugsounet] [API] Please read informations on MagicMirror² screen for continue");
+      console.warn("[Bugsounet] [API [SECRET LINK]", `http://${this.Api.listening}:8085/activate?id=${AdminActivate.id}`);
 
       QRCode.toDataURL(`http://${this.Api.listening}:8085/activate?id=${AdminActivate.id}`, (err, imageUrl) => {
         if (err) return console.error("[Bugsounet] [API] Error generating QR code", err);
